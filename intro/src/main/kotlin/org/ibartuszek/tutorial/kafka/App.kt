67014d6c8934dddd11ed7ff785ef61bd3c182be6
Istvan Bartuszek <istvan.bartuszek@gmail.com>
2022-10-01 12:31:35 +0200

    dev: add message sending/consuming to App

--- intro/src/main/kotlin/org/ibartuszek/tutorial/kafka/App.kt
+++ intro/src/main/kotlin/org/ibartuszek/tutorial/kafka/App.kt
@@ -1,5 +1,8 @@
 package org.ibartuszek.tutorial.kafka
 
+import java.time.Instant
+import java.util.concurrent.Executors
+
 class App {
     val greeting: String
         get() {
@@ -8,5 +11,25 @@ class App {
 }
 
 fun main() {
-    println(App().greeting)
+    val executorService = Executors.newFixedThreadPool(1)
+    val consumer = ConsumerWrapper(Configuration.consumerProperties(), Configuration.topic)
+    executorService.submit(consumer)
+    println("Start producer")
+    ProducerWrapper(Configuration.producerProperties()).apply {
+        val recordMetadata =
+            sendMessage(topic = Configuration.topic, message = "${App().greeting} (${Instant.now()})").get()
+        println(
+            "recordMetadata=${recordMetadata}, " +
+                    "offset=${recordMetadata.offset()}, " +
+                    "timeStamp=${Instant.ofEpochMilli(recordMetadata.timestamp())}, " +
+                    "topic=${recordMetadata.topic()}, " +
+                    "partiotion=${recordMetadata.partition()}, " +
+                    "serializedKeySize=${recordMetadata.serializedKeySize()}, " +
+                    "serializedValueSize=${recordMetadata.serializedValueSize()}"
+        )
+        close()
+    }
+    Thread.sleep(5000L)
+    consumer.close()
+    executorService.shutdown()
 }
