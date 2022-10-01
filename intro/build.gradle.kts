4f403974f6cfac70bcb39323678ebef90bd2fc9e
Istvan Bartuszek <istvan.bartuszek@gmail.com>
2022-10-01 12:29:36 +0200

    dev: add kafka-clients dependency

--- intro/build.gradle.kts
+++ intro/build.gradle.kts
@@ -21,6 +21,9 @@ dependencies {
     // This dependency is used by the application.
     implementation("com.google.guava:guava:30.1.1-jre")
 
+    // https://mvnrepository.com/artifact/org.apache.kafka/kafka-clients
+    implementation("org.apache.kafka:kafka-clients:3.2.3")
+
     // Use the Kotlin test library.
     testImplementation("org.jetbrains.kotlin:kotlin-test")
 
