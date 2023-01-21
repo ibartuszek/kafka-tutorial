a5e887d6e24919a1f845abed3990e8d25ec5fa1e
Istvan Bartuszek <istvan.bartuszek@gmail.com>
2023-01-21 15:25:34 +0100

    dev: use kafka constants in consumer/producer config

--- intro/src/main/kotlin/org/ibartuszek/tutorial/kafka/Configuration.kt
+++ intro/src/main/kotlin/org/ibartuszek/tutorial/kafka/Configuration.kt
@@ -1,28 +1,27 @@
 package org.ibartuszek.tutorial.kafka
 
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.clients.producer.ProducerConfig
 import java.util.*
 
 object Configuration {
 
     const val topic = "helloworld"
+    private const val BOOTSTRAP_SERVERS = "localhost:9092,localhost:9093"
 
     fun producerProperties(): Properties = Properties().apply {
-        addBootstrapServers()
-        put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
-        put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
-    }
-
-    private fun Properties.addBootstrapServers() {
-        put("bootstrap.servers", "localhost:9092,localhost:9093")
+        put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS)
+        put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
+        put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
     }
 
     fun consumerProperties(): Properties = Properties().apply {
-        addBootstrapServers()
-        put("group.id", "helloconsumer")
-        put("enable.auto.commit", "true")
-        put("auto.commit.interval.ms", "1000")
-        put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
-        put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+        put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS)
+        put(ConsumerConfig.GROUP_ID_CONFIG, "helloconsumer")
+        put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true")
+        put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000")
+        put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer")
+        put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer")
     }
 
 }
