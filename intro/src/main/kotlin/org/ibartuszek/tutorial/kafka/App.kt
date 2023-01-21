b1f45b2db493964425a7f56a3eb98ff90ff34ecf
Istvan Bartuszek <istvan.bartuszek@gmail.com>
2023-01-21 15:27:19 +0100

    dev: update example for 10 message sending/consuming

--- intro/src/main/kotlin/org/ibartuszek/tutorial/kafka/App.kt
+++ intro/src/main/kotlin/org/ibartuszek/tutorial/kafka/App.kt
@@ -1,7 +1,13 @@
 package org.ibartuszek.tutorial.kafka
 
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.joinAll
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.apache.kafka.clients.producer.RecordMetadata
 import java.time.Instant
-import java.util.concurrent.Executors
+import java.util.concurrent.CopyOnWriteArrayList
 
 class App {
     val greeting: String
@@ -10,26 +16,55 @@ class App {
         }
 }
 
+private const val RECORDS_SIZE_TO_SEND = 10
+
 fun main() {
-    val executorService = Executors.newFixedThreadPool(1)
-    val consumer = ConsumerWrapper(Configuration.consumerProperties(), Configuration.topic)
-    executorService.submit(consumer)
-    println("Start producer")
-    ProducerWrapper(Configuration.producerProperties()).apply {
-        val recordMetadata =
-            sendMessage(topic = Configuration.topic, message = "${App().greeting} (${Instant.now()})").get()
-        println(
-            "recordMetadata=${recordMetadata}, " +
-                    "offset=${recordMetadata.offset()}, " +
-                    "timeStamp=${Instant.ofEpochMilli(recordMetadata.timestamp())}, " +
-                    "topic=${recordMetadata.topic()}, " +
-                    "partiotion=${recordMetadata.partition()}, " +
-                    "serializedKeySize=${recordMetadata.serializedKeySize()}, " +
-                    "serializedValueSize=${recordMetadata.serializedValueSize()}"
-        )
-        close()
+    runBlocking {
+        val consumer = ConsumerWrapper(Configuration.consumerProperties(), Configuration.topic)
+        val consumedMessages = CopyOnWriteArrayList<ConsumerRecord<String, String>>()
+        launch {
+            consumer.consume {
+                consumedMessages.add(it)
+                logConsumerRecord(it)
+            }
+        }
+        delay(3000) // To reduce the time between the production and consumption of the messages
+
+        val producer = ProducerWrapper(Configuration.producerProperties())
+        val sendingJobs = (1..RECORDS_SIZE_TO_SEND).map {
+            launch {
+                logSentRecords(
+                    producer.sendMessage(topic = Configuration.topic, message = "${App().greeting} (${Instant.now()})")
+                )
+            }
+        }
+        println("Records are sent")
+        sendingJobs.joinAll()
+        println("Closing producer")
+        producer.close()
+
+        while (consumedMessages.size < RECORDS_SIZE_TO_SEND) {
+            println("Waiting for messages...")
+            delay(500)
+        }
+        consumer.close()
+        println("Consumer closed.")
     }
-    Thread.sleep(5000L)
-    consumer.close()
-    executorService.shutdown()
+
+}
+
+private fun logConsumerRecord(it: ConsumerRecord<String, String>) {
+    println("Incoming message at=${Instant.now()}, offset=${it.offset()}, key=${it.key()}, value=${it.value()}")
+}
+
+private fun logSentRecords(recordMetadata: RecordMetadata) {
+    println(
+        "recordMetadata=${recordMetadata}, " +
+                "offset=${recordMetadata.offset()}, " +
+                "timeStamp=${Instant.ofEpochMilli(recordMetadata.timestamp())}, " +
+                "topic=${recordMetadata.topic()}, " +
+                "partiotion=${recordMetadata.partition()}, " +
+                "serializedKeySize=${recordMetadata.serializedKeySize()}, " +
+                "serializedValueSize=${recordMetadata.serializedValueSize()}"
+    )
 }
