4f967670a32782a9da73bee57be87b066e665e12
Istvan Bartuszek <istvan.bartuszek@gmail.com>
2023-01-21 15:26:36 +0100

    dev: use sync producer

--- intro/src/main/kotlin/org/ibartuszek/tutorial/kafka/ProducerWrapper.kt
+++ intro/src/main/kotlin/org/ibartuszek/tutorial/kafka/ProducerWrapper.kt
@@ -4,20 +4,25 @@ import org.apache.kafka.clients.producer.KafkaProducer
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.apache.kafka.clients.producer.RecordMetadata
 import java.util.*
-import java.util.concurrent.Future
+import java.util.concurrent.TimeUnit
 
 class ProducerWrapper(properties: Properties) {
 
     private val producer = KafkaProducer<String, String>(properties)
 
-    fun sendMessage(topic: String, key: String? = null, message: String): Future<RecordMetadata> {
-        val recordMetadata = producer.send(ProducerRecord(topic, key, message))
-        println("Producer has sent key=$key message=$message")
-        return recordMetadata
-    }
+    fun sendMessage(
+        topic: String,
+        key: String? = null,
+        message: String,
+        timeOut: Long = DEFAULT_TIMEOUT
+    ): RecordMetadata = producer.send(ProducerRecord(topic, key, message)).get(timeOut, TimeUnit.MILLISECONDS)
 
     fun close() {
         producer.close()
+    }
+
+    companion object {
+        private const val DEFAULT_TIMEOUT = 1000L
     }
 
 }
