4562df2ff85e8678ced47d970c32d149f0e73f99
Istvan Bartuszek <istvan.bartuszek@gmail.com>
2023-01-21 15:24:56 +0100

    dev: add coroutines dependency

--- intro/build.gradle.kts
+++ intro/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
-    id("org.jetbrains.kotlin.jvm") version "1.5.31"
+    id("org.jetbrains.kotlin.jvm") version "1.6.21"
 
     // Apply the application plugin to add support for building a CLI application in Java.
     application
@@ -12,22 +12,12 @@ repositories {
 }
 
 dependencies {
-    // Align versions of all Kotlin components
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
-
-    // Use the Kotlin JDK 8 standard library.
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-
-    // This dependency is used by the application.
     implementation("com.google.guava:guava:30.1.1-jre")
-
-    // https://mvnrepository.com/artifact/org.apache.kafka/kafka-clients
     implementation("org.apache.kafka:kafka-clients:3.2.3")
-
-    // Use the Kotlin test library.
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
-
-    // Use the Kotlin JUnit integration.
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
 }
 
